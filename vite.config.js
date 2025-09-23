a258af2a7a0139ad861b17448dd9424521fc017d
RaMagure <rahul.m@magureinc.com>
2025-09-23 11:08:14 +0530

    fix .gitignore

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,23 @@ export default defineConfig({
   server: {
     watch: {
       ignored: ["**/node_modules/**", "**/.git/**"],
+      usePolling: false,
+    },
+    fs: {
+      strict: false,
     },
   },
   optimizeDeps: {
     include: ["@mui/material", "@mui/icons-material"],
     exclude: ["@mui/icons-material/esm/*"],
+    force: true,
+  },
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
+  build: {
+    rollupOptions: {
+      maxParallelFileReads: 5,
+    },
   },
 });
